855be171fe2e3853e7af0ee0bd11bd21c5afd814
dcastillog <diego.castilloguajardo@gmail.com>
2020-12-15 18:37:39 -0300

    config(editor): delete prismjs + codemirror integration

--- resources/js/components/editor/Editor.vue
+++ resources/js/components/editor/Editor.vue
@@ -1,63 +1,70 @@
 <template>
-  <prism-editor
-    class="editor"
-    v-model="code"
-    :highlight="highlighter"
-    :readonly="readonly"
-    line-numbers
-  ></prism-editor>
+  <div class="editor">
+    <textarea ref="editor" v-model="code"><p ></p></textarea>
+  </div>
 </template>
 
 <script>
-import { PrismEditor } from "vue-prism-editor";
-import "vue-prism-editor/dist/prismeditor.min.css"; // import the styles somewhere
+import CodeMirror from "codemirror/lib/codemirror.js";
+import "codemirror/lib/codemirror.css";
+// modes
+import "codemirror/mode/xml/xml";
+// themes
+import "codemirror/theme/dracula.css";
 
-// import highlighting library (you can use any library you want just return html string)
-import { highlight, languages } from "prismjs/components/prism-core";
-import "prismjs/components/prism-clike";
-import "prismjs/components/prism-javascript";
-import "prismjs/themes/prism-tomorrow.css"; // import syntax highlighting styles
+import "codemirror/addon/edit/closetag";
+import "codemirror/addon/display/autorefresh";
+
 export default {
-  components: {
-    PrismEditor,
+  mounted: function () {
+    var editor = CodeMirror.fromTextArea(this.$refs["editor"], {
+      mode: this.mode,
+      theme: this.theme,
+      lineNumbers: this.lineNumbers,
+      tabSize: this.tabSize,
+      readOnly: this.readOnly,
+      autoCloseTags: this.autoCloseTags,
+      autoRefresh: true,
+    });
   },
+
+  data() {
+    return {
+      code: "",
+    };
+  },
+
   props: {
-    readonly: {
+    mode: {
+      type: String,
+      default: "xml",
+    },
+    theme: {
+      type: String,
+      default: "dracula",
+    },
+    lineNumbers: {
+      type: Boolean,
+      default: true,
+    },
+    autoCloseTags: {
+      type: Boolean,
+      default: true,
+    },
+    tabSize: {
+      type: Number,
+      default: 4,
+    },
+    readOnly: {
       type: Boolean,
       default: false,
-    },
-  },
-  data: () => ({ code: "" }),
-  methods: {
-    highlighter(code) {
-      console.log("code", code);
-      return highlight(code, languages.js); // languages.<insert language> to return html with markup
     },
   },
 };
 </script>
 
 <style>
-/* required class */
-.editor {
-  /* we dont use `language-` classes anymore so thats why we need to add background and text color manually */
-  background: #2d2d2d;
-  color: white;
-
-  /* you must provide font-family font-size line-height. Example: */
-  font-family: Fira code, Fira Mono, Consolas, Menlo, Courier, monospace;
-  font-size: 14px;
-  line-height: 1.5;
-  padding: 5px;
-}
-
-/* optional class for removing the outline */
-.prism-editor__textarea:focus {
-  outline: none;
-}
-
-.prism-editor-wrapper .prism-editor__editor,
-.prism-editor-wrapper .prism-editor__textarea {
-  color: white;
+.CodeMirror-code {
+  font-family: Menlo, Monaco, Consolas, "Courier New", monospace;
 }
 </style>

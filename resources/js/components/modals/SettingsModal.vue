1b85790486f3d76a96e8cf94d9618fb723fe93fb
dcastillog <diego.castilloguajardo@gmail.com>
2020-12-16 13:14:19 -0300

    feat: add tags on snippet-card + settings modal

---
+++ resources/js/components/modals/SettingsModal.vue
@@ -0,0 +1,49 @@
+<template>
+  <b-modal id="settings-modal">
+    <template #modal-header> Settings </template>
+    <div class="d-block">
+      <b-form>
+        <form-group>
+          <template #form-label>Github Enterprise Users Only</template>
+          <template #form-input
+            ><b-form-input
+              id="name"
+              type="text"
+              required
+              placeholder="https://github.host/api/v"
+            ></b-form-input
+          ></template>
+        </form-group>
+        <form-group>
+          <template #form-label>Personal Access Token</template>
+          <template #form-input
+            ><b-form-input
+              id="acesssToken"
+              type="text"
+              required
+              placeholder="Enter Github personal access token"
+            ></b-form-input
+          ></template>
+        </form-group>
+      </b-form>
+    </div>
+
+    <template #modal-ok>
+      <button>Save</button>
+    </template>
+
+    <template #modal-cancel>
+      <button>Cancel</button>
+    </template>
+  </b-modal>
+</template>
+
+<script>
+import FormGroup from "../forms/FormGroup";
+
+export default {
+  components: {
+    FormGroup,
+  },
+};
+</script>
--- resources/js/components/snippets/SnippetCard.vue
+++ resources/js/components/snippets/SnippetCard.vue
@@ -2,6 +2,7 @@
   <b-card class="m-4">
     <div class="snippet-card__header">
       <h5>Card title</h5>
+
       <div class="snippet-card__header-buttons">
         <icon-button>
           <icon-github></icon-github>
@@ -15,10 +16,34 @@
       </div>
     </div>
     <b-card-text>
+      Descripcion
       <div class="snippet-card__content">
-        <strong>scripts.js <span>(Javascript)</span></strong>
-        <div>
-          <textarea name="" id="" cols="30" rows="1"></textarea>
+        <div class="row">
+          <div class="col-sm-8 col-md-9 col-lg-10">
+            <strong>scripts<span>(text)</span></strong>
+          </div>
+          <div class="col text-center">
+            <icon-button @click="makeToast()">
+              <icon-clipboard></icon-clipboard>
+            </icon-button>
+            <icon-button>
+              <icon-photograph></icon-photograph>
+            </icon-button>
+          </div>
+        </div>
+        <editor readOnly></editor>
+        <div class="snippet-card__tags row">
+          <span class="tag"># Web development</span>
+          <span class="tag"># Web Mobile</span>
+          <span class="tag"># Web Mobile</span>
+          <span class="tag"># Web Mobile</span>
+          <span class="tag"># Web Mobile</span>
+          <span class="tag"># IA</span>
+          <span class="tag"># IA</span>
+          <span class="tag"># IA</span>
+          <span class="tag"># IA</span>
+          <span class="tag"># IA</span>
+          <span class="tag"># IA</span>
         </div>
       </div>
     </b-card-text>
@@ -31,7 +56,25 @@
   justify-content: space-between;
 }
 .snippet-card__content {
+  background: #eeecec;
+}
+.snippet-card__tags {
+  padding: 1.5rem;
+}
+.tag {
+  padding: 0.5rem;
+  margin: 5px;
   background: #cacaca;
+  color: gray;
+  border-radius: 8px;
+  box-shadow: 5px 5px 5px #aaaaaa;
+  cursor: pointer;
+
+  &:hover {
+    background: lighten($color: gray, $amount: 5);
+    color: #cacaca;
+    transition: all 0.2s ease;
+  }
 }
 </style>
 
@@ -40,13 +83,35 @@ import IconButton from "../buttons/IconButton";
 import IconGithub from "../icons/IconGithub";
 import IconPencil from "../icons/IconPencil";
 import IconTrash from "../icons/IconTrash";
+import IconPhotograph from "../icons/IconPhotograph";
+import IconClipboard from "../icons/IconClipboard";
+import Editor from "../editor/Editor";
 
 export default {
   components: {
+    Editor,
     IconButton,
     IconGithub,
     IconPencil,
     IconTrash,
+    IconPhotograph,
+    IconClipboard,
+  },
+  data() {
+    return {
+      toastCount: 0,
+    };
+  },
+  methods: {
+    makeToast(append = false) {
+      this.toastCount++;
+      this.$bvToast.toast("-", {
+        title: "Copied clipboard",
+        autoHideDelay: 100,
+        toaster: "b-toaster-bottom-left",
+      });
+    },
   },
 };
 </script>
+

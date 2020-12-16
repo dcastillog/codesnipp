c3298d7c5e3b6d28e7393fcf9d2c7a45bfd49a95
dcastillog <diego.castilloguajardo@gmail.com>
2020-12-16 13:12:42 -0300

    feat(snippet-form): change selected language and theme on select

--- resources/js/components/editor/Editor.vue
+++ resources/js/components/editor/Editor.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="editor">
-    <textarea ref="editor" v-model="code"><p ></p></textarea>
+    <textarea ref="editor"></textarea>
   </div>
 </template>
 
@@ -9,35 +9,94 @@ import CodeMirror from "codemirror/lib/codemirror.js";
 import "codemirror/lib/codemirror.css";
 // modes
 import "codemirror/mode/xml/xml";
+import "codemirror/mode/javascript/javascript";
+import "codemirror/mode/cobol/cobol";
+import "codemirror/mode/coffeescript/coffeescript";
+import "codemirror/mode/css/css";
+import "codemirror/mode/dart/dart";
+import "codemirror/mode/django/django";
+import "codemirror/mode/dockerfile/dockerfile";
+import "codemirror/mode/erlang/erlang";
+import "codemirror/mode/go/go";
+import "codemirror/mode/markdown/markdown";
+import "codemirror/mode/pascal/pascal";
+import "codemirror/mode/perl/perl";
+import "codemirror/mode/php/php";
+import "codemirror/mode/pug/pug";
+import "codemirror/mode/python/python";
+import "codemirror/mode/r/r";
+import "codemirror/mode/ruby/ruby";
+import "codemirror/mode/rust/rust";
+import "codemirror/mode/sass/sass";
+import "codemirror/mode/clike/clike";
 // themes
 import "codemirror/theme/dracula.css";
+import "codemirror/theme/3024-day.css";
+import "codemirror/theme/3024-night.css";
+import "codemirror/theme/ambiance.css";
+import "codemirror/theme/ayu-mirage.css";
+import "codemirror/theme/base16-dark.css";
+import "codemirror/theme/base16-light.css";
+import "codemirror/theme/bespin.css";
+import "codemirror/theme/cobalt.css";
+import "codemirror/theme/darcula.css";
+import "codemirror/theme/duotone-dark.css";
+import "codemirror/theme/duotone-light.css";
 
 import "codemirror/addon/edit/closetag";
 import "codemirror/addon/display/autorefresh";
 
 export default {
   mounted: function () {
-    var editor = CodeMirror.fromTextArea(this.$refs["editor"], {
-      mode: this.mode,
-      theme: this.theme,
+    this.editor = CodeMirror.fromTextArea(this.$refs["editor"], {
+      mode: this.mode.toLowerCase(),
+      theme: this.theme.toLowerCase(),
       lineNumbers: this.lineNumbers,
       tabSize: this.tabSize,
       readOnly: this.readOnly,
       autoCloseTags: this.autoCloseTags,
       autoRefresh: true,
     });
+
+    this.editor.on("changes", () => {
+      this.$emit("input", this.content);
+      this.content = this.editor.getValue();
+    });
+
+    this.editor.save();
   },
 
+  watch: {
+    mode: function (newValue) {
+      if (
+        newValue.toLowerCase() === "java" ||
+        newValue.toLowerCase() === "c" ||
+        newValue.toLowerCase() === "cpp" ||
+        newValue.toLowerCase() === "cs" ||
+        newValue.toLowerCase() === "kotlin"
+      ) {
+        console.log("clike", newValue);
+        this.editor.setOption("mode", "clike");
+      } else {
+        this.editor.setOption("mode", newValue.toLowerCase());
+      }
+    },
+
+    theme: function (newValue, oldValue) {
+      this.editor.setOption("theme", newValue.toLowerCase());
+    },
+  },
   data() {
     return {
-      code: "",
+      content: "",
+      editor: null,
     };
   },
 
   props: {
     mode: {
       type: String,
-      default: "xml",
+      default: "javascript",
     },
     theme: {
       type: String,
@@ -60,11 +119,22 @@ export default {
       default: false,
     },
   },
+  methods: {
+    updateValue(value) {
+      console.log(value);
+      this.$emit("input", value);
+    },
+  },
 };
 </script>
 
 <style>
 .CodeMirror-code {
   font-family: Menlo, Monaco, Consolas, "Courier New", monospace;
+}
+.CodeMirror {
+  border-radius: 10px;
+  box-shadow: 0 0 0 1px #444444;
+  border: 5px solid transparent;
 }
 </style>
--- resources/js/components/forms/CreateSnippetForm.vue
+++ resources/js/components/forms/CreateSnippetForm.vue
@@ -1,64 +1,123 @@
 <template>
   <b-form>
-    <form-group>
+    <form-group inline>
       <template #form-label>Name</template>
       <template #form-input
         ><b-form-input
           id="name"
           type="text"
+          v-model="snippet.name"
           required
           placeholder="Enter snippet name"
         ></b-form-input
       ></template>
     </form-group>
-    <form-group>
+    <form-group inline>
       <template #form-label>Description</template>
       <template #form-input
         ><b-form-input
           id="description"
           type="text"
+          v-model="snippet.description"
           required
           placeholder="Enter description"
         ></b-form-input
       ></template>
     </form-group>
-    <form-group>
+    <form-group inline>
       <template #form-label>Tags</template>
       <template #form-input>
-        <b-form-tags id="tags" placeholder="Enter tags"></b-form-tags>
+        <b-form-tags
+          id="tags"
+          v-model="snippet.tags"
+          placeholder="Enter tags"
+        ></b-form-tags>
       </template>
     </form-group>
 
-    <div class="file-section">
-      <form-group>
-        <template #form-label>Filename</template>
-        <template #form-input
-          ><b-form-input
-            id="filename"
-            type="text"
-            required
-            placeholder="Enter file name"
-          ></b-form-input
-        ></template>
-      </form-group>
-      <form-group>
-        <template #form-label>Language</template>
-        <template #form-input>
-          <v-select :options="[{ label: 'Python', code: 'py' }]"></v-select>
-        </template>
-      </form-group>
-      <form-group>
-        <template #form-label>Snippet</template>
-        <template #form-input>
-          <editor></editor>
-        </template>
-      </form-group>
+    <!-- <form-group inline>
+      <template #form-label>Visibility</template>
+      <template #form-input>
+        <b-form-select v-model="snippet.public">
+          <b-form-select-option :value="null"
+            >Please select an option</b-form-select-option
+          >
+          <b-form-select-option :value="false">Secret</b-form-select-option>
+          <b-form-select-option :value="true">Public</b-form-select-option>
+        </b-form-select>
+      </template>
+    </form-group> -->
+
+    <div class="file-section" v-for="(file, index) in files" :key="index">
+      <div class="row">
+        <div class="col">
+          <form-group inline>
+            <template #form-label>Filename</template>
+            <template #form-input
+              ><b-form-input
+                id="filename"
+                v-model="file.name"
+                type="text"
+                required
+                placeholder="Enter file name"
+              ></b-form-input
+            ></template>
+          </form-group>
+
+          <form-group inline>
+            <template #form-label>Config</template>
+            <template #form-input>
+              <b-row>
+                <b-col sm="12" lg="6" class="pt-3"
+                  ><v-select
+                    label="name"
+                    :options="themeOptions"
+                    placeholder="Select a theme"
+                    v-model="file.theme"
+                  ></v-select
+                ></b-col>
+                <b-col sm="12" lg="6" class="pt-3"
+                  ><v-select
+                    label="name"
+                    :options="languageOptions"
+                    placeholder="Select a language"
+                    v-model="file.language"
+                  ></v-select
+                ></b-col>
+              </b-row>
+            </template>
+          </form-group>
+
+          <form-group inline>
+            <template #form-label>Snippet</template>
+            <template #form-input>
+              <editor
+                v-model="file.content"
+                :theme="file.theme"
+                :mode="file.language"
+              ></editor>
+            </template>
+          </form-group>
+          <div class="text-center">
+            <b-button @click="addFile" v-show="files.length - 1 == index"
+              >Add a file</b-button
+            >
+          </div>
+        </div>
+        <div class="col-1">
+          <icon-button v-show="files.length > 1" @click="deleteFile(index)">
+            <icon-trash></icon-trash>
+          </icon-button>
+        </div>
+      </div>
     </div>
   </b-form>
 </template>
 
 
 <script>
+import IconButton from "../buttons/IconButton";
+import IconTrash from "../icons/IconTrash";
 import FormGroup from "./FormGroup";
 import Editor from "../editor/Editor";
 import vSelect from "vue-select";
@@ -66,9 +125,53 @@ import "vue-select/dist/vue-select.css";
 
 export default {
   components: {
+    IconButton,
+    IconTrash,
     FormGroup,
     Editor,
     vSelect,
+  },
+  data() {
+    return {
+      snippet: {
+        name: "",
+        description: "",
+        files: {},
+        public: false,
+        // updatedAt: null,
+        // createdAt: null
+        tags: [],
+      },
+      files: [
+        {
+          name: "",
+          theme: "",
+          language: "",
+          content: "",
+        },
+      ],
+
+      themeOptions: ["Ambiance", "Dracula"],
+
+      languageOptions: ["Plain text", "Java", "Javascript"],
+    };
+  },
+
+  methods: {
+    addFile: function () {
+      this.files.push({
+        name: "",
+        theme: "",
+        language: "",
+        content: "",
+      });
+    },
+    deleteFile: function (index) {
+      this.files.splice(index, 1);
+    },
+    updateCode: function (updatedCode) {
+      console.log(updatedCode);
+    },
   },
 };
 </script>
